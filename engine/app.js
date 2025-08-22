f997aba8912db715f6c8e34eed9a3ccf387ef8cc
stevenfojas670 <84162024+stevenfojas670@users.noreply.github.com>
2025-08-22 00:23:14 -0700

    Split main.html and added app.js, styles.css and installed bootstrap.

---
+++ engine/app.js
@@ -0,0 +1,68 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+let lastTime = performance.now();
+let currentTime = 0;
+let deltaTime = 0;
+
+let mousePos = [0, 0];
+let mouseDownLeft = false;
+let mouseDownRight = false;
+
+let simulation = new Simulation();
+
+// Calculating the mouse position in the canvas
+function getMousePos(canvas, evt) {
+	var rect = canvas.getBoundingClientRect();
+	return { x: evt.clientX - rect.left, y: evt.clientY - rect.top };
+}
+
+// Event listener for keys being pressed
+window.addEventListener('keydown', (evt) => {
+	simulation.onKeyboardPressed(evt);
+});
+
+// Event listener for mouse position within the canvas
+canvas.addEventListener('mousemove', (evt) => {
+	mouse = getMousePos(canvas, evt);
+	mousePos = [mouse.x, mouse.y];
+
+	//console.log(mousePos)
+});
+
+window.addEventListener('mousedown', (evt) => {
+	if (evt.button === 0) mouseDownLeft = true;
+	if (evt.button === 2) mouseDownRight = true;
+
+	//console.log(mouseDownLeft)
+});
+
+window.addEventListener('mouseup', (evt) => {
+	if (evt.button === 0) mouseDownLeft = false;
+	if (evt.button === 2) mouseDownRight = false;
+
+	//console.log(mouseDownLeft)
+});
+
+mainLoop();
+
+function updateSimulation(deltaTime) {
+	simulation.update(deltaTime);
+	Clear();
+	simulation.draw(ctx);
+}
+
+function mainLoop() {
+	window.requestAnimationFrame(mainLoop);
+
+	currentTime = performance.now();
+	deltaTime = (currentTime - lastTime) / 1000;
+	updateSimulation(deltaTime);
+
+	lastTime = currentTime;
+}
+
+function Clear() {
+	ctx.fillStyle = 'rgb(240, 240, 240)';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
